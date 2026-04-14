f2d7a852e210ea9e8a9dcd9bae5c6b9f4af59b87
Doverholm <konton.renderer824@passmail.net>
2026-04-14 21:09:30 +0200

    La till ethereum_ore_block och ethereum_block

--- src/main/java/net/doverholm/spmod/block/ModBlocks.java
+++ src/main/java/net/doverholm/spmod/block/ModBlocks.java
@@ -1,10 +1,12 @@
 package net.doverholm.spmod.block;
 
 import net.doverholm.spmod.SPMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -14,6 +16,8 @@ public class ModBlocks {
 
     public static final Block ETHEREUM_ORE_BLOCK = registerBlock("ethereum_ore_block",
             new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block ETHEREUM_BLOCK = registerBlock("ethereum_block",
+            new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -27,5 +31,10 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         SPMod.LOGGER.info("Registiring Mod Blocks  " + SPMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.ETHEREUM_ORE_BLOCK);
+            entries.add(ModBlocks.ETHEREUM_BLOCK);
+        });
     }
 }
