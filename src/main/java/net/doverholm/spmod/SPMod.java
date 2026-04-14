f024aca8e2e440b783ba330c66fa9342ecedddd0
Doverholm <konton.renderer824@passmail.net>
2026-04-14 18:40:26 +0200

    Testar git med kommentar

--- src/main/java/net/doverholm/spmod/SPMod.java
+++ src/main/java/net/doverholm/spmod/SPMod.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// En kommentar ifrån Martin :)
+
 public class SPMod implements ModInitializer {
 	public static final String MOD_ID = "spmod";
 
