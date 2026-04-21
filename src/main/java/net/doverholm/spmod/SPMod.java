61fc2e11508f852f8b0fcac637bdbf4785ef6fab
Doverholm <konton.renderer824@passmail.net>
2026-04-21 22:00:15 +0200

    test

--- src/main/java/net/doverholm/spmod/SPMod.java
+++ src/main/java/net/doverholm/spmod/SPMod.java
@@ -1,31 +1,21 @@
 package net.doverholm.spmod;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
 import net.doverholm.spmod.component.ModDataComponentTypes;
 import net.doverholm.spmod.item.ModItemGroups;
 import net.doverholm.spmod.block.ModBlocks;
 import net.doverholm.spmod.item.ModItems;
 import net.doverholm.spmod.world.gen.ModWorldGeneration;
-import net.doverholm.spmod.rank.Rank;
-import net.doverholm.spmod.rank.RankManager;
-import net.doverholm.spmod.util.NameTagUtil;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.server.command.CommandManager;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,7 +57,6 @@ public class SPMod implements ModInitializer {
 		}));
 
 
-		ServerTickEvents.END_SERVER_TICK.register(server -> {
 		ServerTickEvents.END_SERVER_TICK.register((server) -> {
 			for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
 
@@ -92,57 +81,5 @@ public class SPMod implements ModInitializer {
 			}
 		});
 
-			}
-
-
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			dispatcher.register(CommandManager.literal("setrank")
-					.requires(src -> src.hasPermissionLevel(2))
-					.then(CommandManager.argument("player", EntityArgumentType.player())
-							.then(CommandManager.argument("rank", StringArgumentType.word())
-									.executes(context -> {
-
-										ServerPlayerEntity target = EntityArgumentType.getPlayer(context, "player");
-										String rankStr = StringArgumentType.getString(context, "rank");
-
-										Rank rank = Rank.valueOf(rankStr.toUpperCase());
-
-										RankManager.setRank(target.getUuid(), rank);
-										NameTagUtil.updateName(target);
-
-										context.getSource().sendFeedback(
-												() -> Text.literal("Rank satt!"), false
-										);
-
-										return 1;
-									}))));
-		});
-
-
-		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
-
-			Rank rank = RankManager.getRank(sender.getUuid());
-
-			Text prefix = switch (rank) {
-				case ADMIN -> Text.literal("[ADMIN] ").formatted(Formatting.RED);
-				case MODERATOR -> Text.literal("[MOD] ").formatted(Formatting.BLUE);
-				default -> Text.literal("");
-			};
-
-			Text newMessage = Text.literal("")
-					.append(prefix)
-					.append(sender.getName())
-					.append(Text.literal(": "))
-					.append(message.getContent());
-
-			sender.getServer().getPlayerManager().broadcast(newMessage, false);
-
-			return false;
-		});
-
-
-		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-			NameTagUtil.updateName(handler.getPlayer());
-		});
 	}
 }
